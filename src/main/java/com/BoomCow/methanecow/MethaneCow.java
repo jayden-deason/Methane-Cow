72274097003198d2af07946d4bec8402972870c8
jayden-deason <ironfist4657@gmail.com>
2023-05-12 02:05:46 -0500

    Removed the Cancer (options was not real)

--- src/main/java/com/BoomCow/methanecow/MethaneCow.java
+++ src/main/java/com/BoomCow/methanecow/MethaneCow.java
@@ -49,7 +49,6 @@ public class MethaneCow
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
