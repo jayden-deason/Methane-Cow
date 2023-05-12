3da26508a6cae6d8dc98418d01f691556e428218
AMPerez04 <a.m.perez@wustl.edu>
2023-05-12 01:45:42 -0500

    added mappings

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -19,10 +19,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("methanecow")
 public class ExampleMod
 {
     // Directly reference a log4j logger.
+    public static final String MOD_ID = "methanecow";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ExampleMod() {
