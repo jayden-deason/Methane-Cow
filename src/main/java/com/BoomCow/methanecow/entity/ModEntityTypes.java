8de9417b3e03a8b68df2bbf8a8896317c9bc4276
jayden-deason <ironfist4657@gmail.com>
2023-10-04 03:34:15 -0400

    Lowered chance of emissions and explosions

--- src/main/java/com/BoomCow/methanecow/entity/ModEntityTypes.java
+++ src/main/java/com/BoomCow/methanecow/entity/ModEntityTypes.java
@@ -1,9 +1,7 @@
 package com.BoomCow.methanecow.entity;
 
 import com.BoomCow.methanecow.MethaneCow;
-//import com.BoomCow.methanecow.entity.custom.BabyMethaneCowEntity;
 import com.BoomCow.methanecow.entity.custom.MethaneCowEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
@@ -20,11 +18,6 @@ public class ModEntityTypes {
             ENTITY_TYPES.register("methane_cow", () -> EntityType.Builder.create(MethaneCowEntity::new,
                     EntityClassification.CREATURE).size(0.9f, 1.4f).build(new ResourceLocation(MethaneCow.MOD_ID,
                     "methane_cow").toString()));
-
-//    public static final RegistryObject<EntityType<BabyMethaneCowEntity>> BABY_METHANE_COW =
-//            ENTITY_TYPES.register("baby_methane_cow", () -> EntityType.Builder.create(BabyMethaneCowEntity::new,
-//                    EntityClassification.CREATURE).size(0.9f, 1.4f).build(new ResourceLocation(MethaneCow.MOD_ID,
-//                    "baby_methane_cow").toString()));
 
     public static void register(IEventBus eventBus) {
         ENTITY_TYPES.register(eventBus);
