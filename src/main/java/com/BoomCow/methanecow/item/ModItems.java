5775125bf32ced200372220eae169e30c6907355
jayden-deason <ironfist4657@gmail.com>
2024-02-13 01:54:43 -0500

    Initial implementation for Flavored Milk Added milk behavior and a test milk

--- src/main/java/com/BoomCow/methanecow/item/ModItems.java
+++ src/main/java/com/BoomCow/methanecow/item/ModItems.java
@@ -3,14 +3,18 @@ package com.BoomCow.methanecow.item;
 
 import com.BoomCow.methanecow.MethaneCow;
 import com.BoomCow.methanecow.entity.ModEntityTypes;
+import com.BoomCow.methanecow.item.custom.FlavoredMilkItem;
 import com.BoomCow.methanecow.item.custom.ModSpawnEggItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.MilkBucketItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import static net.minecraft.item.Items.BUCKET;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -20,6 +24,8 @@ public class ModItems {
             () -> new ModSpawnEggItem(ModEntityTypes.METHANE_COW, 4470310, 10592673,
                     new Item.Properties().group(ItemGroup.MISC)));
 
+    public static final RegistryObject<FlavoredMilkItem> SPEED_MILK_BUCKET = ITEMS.register("speed_milk_bucket", () ->
+            new FlavoredMilkItem((new Item.Properties()).containerItem(BUCKET).maxStackSize(1).group(ItemGroup.MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
