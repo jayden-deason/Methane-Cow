f638050f4b4486903f333634b81d3f85b67af07c
jayden-deason <ironfist4657@gmail.com>
2024-02-14 16:04:48 -0500

    Implemented data generators for simple items Added textures for flavored milks

--- src/main/java/com/BoomCow/methanecow/datagen/DataGenerators.java
+++ src/main/java/com/BoomCow/methanecow/datagen/DataGenerators.java
@@ -14,6 +14,6 @@ public class DataGenerators {
         DataGenerator generator = event.getGenerator();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
-
+        generator.addProvider(new ModItemModelProvider(generator, existingFileHelper));
     }
 }
--- src/main/java/com/BoomCow/methanecow/datagen/ModItemModelProvider.java
+++ src/main/java/com/BoomCow/methanecow/datagen/ModItemModelProvider.java
@@ -4,6 +4,7 @@ import com.BoomCow.methanecow.MethaneCow;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -14,10 +15,36 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-
+        simpleItem("speed_milk_bucket");
+        simpleItem("slowness_milk_bucket");
+        simpleItem("haste_milk_bucket");
+        simpleItem("mining_fatigue_milk_bucket");
+        simpleItem("strength_milk_bucket");
+        simpleItem("instant_health_milk_bucket");
+        simpleItem("instant_damage_milk_bucket");
+        simpleItem("jump_boost_milk_bucket");
+        simpleItem("nausea_milk_bucket");
+        simpleItem("regeneration_milk_bucket");
+        simpleItem("resistance_milk_bucket");
+        simpleItem("fire_resistance_milk_bucket");
+        simpleItem("water_breathing_milk_bucket");
+        simpleItem("invisibility_milk_bucket");
+        simpleItem("blindness_milk_bucket");
+        simpleItem("night_vision_milk_bucket");
+        simpleItem("hunger_milk_bucket");
+        simpleItem("weakness_milk_bucket");
+        simpleItem("poison_milk_bucket");
+        simpleItem("wither_milk_bucket");
+        simpleItem("saturation_milk_bucket");
+        simpleItem("glowing_milk_bucket");
+        simpleItem("levitation_milk_bucket");
+        simpleItem("luck_milk_bucket");
+        simpleItem("unluck_milk_bucket");
+        simpleItem("slow_falling_milk_bucket");
     }
 
-    private ItemModelProvider simpleItem(String name) {
-        return getBuilder(item.getName()).;
+    private ItemModelBuilder simpleItem(String name) {
+        return getBuilder(name).parent(getExistingFile(mcLoc("item/generated"))).texture("layer0",
+                "item/" + name);
     }
 }
